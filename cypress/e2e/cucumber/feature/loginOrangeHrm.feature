20f8f8c4201023e62644d18bfea640e9078998ff
penzarmerchant <penzarmerchant28@gmail.com>
2024-12-19 18:20:30 +0530

    Added login Negative scenario Test

--- cypress/e2e/cucumber/feature/loginOrangeHrm.feature
+++ cypress/e2e/cucumber/feature/loginOrangeHrm.feature
@@ -6,3 +6,11 @@ Given User is on the login page
 When User enters valid credentials 
 And User clicks on login button
 Then User will be navigated to the dashboard page
+
+
+Scenario: Login with InValid Credentials
+
+Given User is on the login page
+When User enters invalid credentials 
+And User clicks on login button
+Then User will view an error message
--- cypress/e2e/cucumber/stepDefinitions/loginOrangeHrm/loginOrangeHrm_stepdef.js
+++ cypress/e2e/cucumber/stepDefinitions/loginOrangeHrm/loginOrangeHrm_stepdef.js
@@ -11,10 +11,22 @@ When(/^User enters valid credentials$/, () => {
     cy.get('input[name="password"]').type('admin123');
 });
 
+
+When(/^User enters invalid credentials$/, () => {
+	cy.get('input[name="username"]').type('AdminA');
+    cy.get('input[name="password"]').type('admin1');
+});
+
+
 When(/^User clicks on login button$/, () => {
         cy.get('button[type="submit"]').click();
 });
 
 Then(/^User will be navigated to the dashboard page$/, () => {
 	cy.get('.oxd-topbar-header-breadcrumb').should('be.visible');
+});
+
+
+Then(/^User will view an error message$/, () => {
+	cy.get('.oxd-alert-content-text').should('be.visible');
 });
